5673af8910fea273482a5c48042f4fae9fb7c2b7
Wesam Abo Aqel <wesamabuaqel138@gmail.com>
2026-02-15 15:12:25 +0300

    internship-tutorial

---
+++ source/sequalize.ts
@@ -0,0 +1,31 @@
+import { Sequelize } from '@sequelize/core';
+import { MySqlDialect } from '@sequelize/mysql';
+import { User } from '../models/user.model.js';
+
+
+// export const sequelize = new Sequelize( {
+//   dialect: MySqlDialect,
+//   database: "railway",
+//   user: "root",
+//   password: "IJmbRFpbFLDgAMxpsxPhrgsayuPYaXcz",
+//   host: "nozomi.proxy.rlwy.net",
+//   port: 3306,
+// });
+
+const sequelize = new Sequelize({
+    url: process.env.DATABASE_URL!,
+    dialect: "mysql",
+    logging: false,
+    models: [User],
+});
+
+export const testConnection = async () => {
+  try {
+    // console.log(process.env.DATABASE_URL)
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log("MySQL connected successfully.");
+  } catch (error) {
+    console.error("Unable to connect to MySQL:", error);
+  }
+};
